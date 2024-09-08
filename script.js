3e1fab30f6336831a2eadaf6e2599f1c0a54a40c
Vy Xuân Phúc <100058443+ItzBlack6093@users.noreply.github.com>
2024-09-08 21:28:04 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1381,17 +1381,5 @@ else {
   a.innerHTML = `\n\t\t\t\t\t\t<div class="tetra_modal_record_header">\n\t\t\t\t\t\t\t<h6>${dataset.Strings.longTypeNames.zenithex}</h6>\n\t\t\t\t\t\t\t<div class="standingset">\n\t\t\t\t\t\t\t\t${t.rank_local ? `\n\t\t\t\t\t\t\t\t\t<div class="standingset_local">${s} #<span>${t.rank_local.toLocaleString("en-US")}</span></div>\n\t\t\t\t\t\t\t\t` : ""}\n\t\t\t\t\t\t\t\t ${t.rank ? `\n\t\t\t\t\t\t\t\t\t<div class="standingset_global ${1 === t.rank ? "t1" : ""}" data-digits="${(t.rank - 1).toString().length}">#<span>${t.rank.toLocaleString("en-US")}</span></div>\n\t\t\t\t\t\t\t\t` : ""}\n\t\t\t\t\t\t\t</div>\n\t\t\t\t\t\t</div>\n\t\t\t\t\t\t ${i ? `\n\t\t\t\t\t\t\t<h5 title="This week">${Math.floor(i.results.stats.zenith.altitude).toLocaleString("en-US")}<span class="ms">.${Math.floor(i.results.stats.zenith.altitude % 1 * 10)}</span><span class="unit">M</span></h5>\n\t\t\t\t\t\t` : `\n\t\t\t\t\t\t\t<h5 title="Not this week - player has not played this week" class="revolvedscore">${Math.floor(o.results.stats.zenith.altitude).toLocaleString("en-US")}<span class="ms">.${Math.floor(o.results.stats.zenith.altitude % 1 * 10)}</span><span class="unit">M</span></h5>\n\t\t\t\t\t\t`}\n\t\t\t\t\t\t ${i && o ? `\n\t\t\t\t\t\t\t<h3 title="Updates every week">career best <span>${Math.floor(o.results.stats.zenith.altitude).toLocaleString("en-US")}.${Math.floor(o.results.stats.zenith.altitude % 1 * 10)}</span>${Ml("M")} (#${t.best.rank.toLocaleString("en-US")})</h3>\n\t\t\t\t\t\t` : `\n\t\t\t\t\t\t\t<h3><span>${Bt(Date.parse((i ?? o).ts)).toUpperCase()}</span> ago</h3>\n\t\t\t\t\t\t`}`,
   e.appendChild(a)  
   }
-
-  {
-    const t = outputRecord.data.zen
-      , i = t.score
-      , l = t.level
-      , o = document.createElement("div");
-    o.classList.add("tetra_modal_record"),
-    o.classList.add("flex-item"),
-    o.innerHTML = `\n\t\t\t\t\t\t<div class="tetra_modal_record_header">\n\t\t\t\t\t\t\t<h6>${dataset.Strings.longTypeNames.zen}</h6><h6>${Hiero(t.level)}</h6>\n\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t</div>\n\t\t\t\t\t\t</div>\n\t\t\t\t\t\t<h5>${i.toLocaleString("en-US")}</h5>\n\t\t\t\t\t\t<h3>Level<span> ${l}</span></h3>`,
-    e.appendChild(o)
-}
-
 }
 }
